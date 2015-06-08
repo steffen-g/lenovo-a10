8b776ef57564c49f4fa5a63c20003fe7e7612945
Steffen Graf <steffen@gsg-elektronik.de>
2015-06-08 17:46:56 +0200

    fixed make script

--- scripts/make_initrd_kernel_flash.sh
+++ scripts/make_initrd_kernel_flash.sh
@@ -2,7 +2,7 @@
 cd ../kernel/
 make Image -j4
 sudo update-initramfs -u -v -k 3.0.36
-cp -v initrd.img-3.0.36 ../initrd/
+cp -v /boot/initrd.img-3.0.36 ../initrd/
 cd ../scripts
 ls -al ../initrd/*
 ls -al ../out/*.img
